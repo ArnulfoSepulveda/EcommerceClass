6ba97dea9ada376af264daf6ab3d150009b923c0
ArnulfoYumali <asepulveda@yumali.com>
2016-08-13 17:47:11 -0500

    Signed-off-by: ArnulfoYumali <asepulveda@yumali.com>

--- server.js
+++ server.js
@@ -7,14 +7,22 @@ var ejsMate = require('ejs-mate');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var flash = require('express-flash');
+var MongoStore = require('connect-mongo/es5')(session);
+var passport = require('passport');
+
 
 // User defined 
 var secret = require('./config/secret');
 var User = require('./models/user');
+var Category = require('./models/category');
+
 var mainRoutes = require('./routes/main');
 var userRoutes = require('./routes/user');
+var adminRoutes = require('./routes/admin');
+var apiRoutes = require('./api/api');
 
 var app = express();
+
 mongoose.connect(secret.database, function(err){
     if(err){
         console.log(err);
@@ -22,6 +30,8 @@ mongoose.connect(secret.database, function(err){
         console.log("Connected to database");
     }
 });
+///
+
 
 //Middleware
 app.use(express.static(__dirname+'/public'));
@@ -32,17 +42,34 @@ app.use(cookieParser());
 app.use(session({
     resave: true,
     saveUninitialized: true,
-    secret: secret.secretKey
+    secret: secret.secretKey,
+    store: new MongoStore({url: secret.database, autoReconnect: true})
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next){
+    Category.find({}, function(err, categories){
+        if(err) return next(err);
+        res.locals.categories = categories;
+        next();
+    });
+});
+
+app.use(function(req, res, next){
+    res.locals.user = req.user;
+    next();
+});
+
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 
 app.use(mainRoutes);
 app.use(userRoutes);
-
-
+app.use(adminRoutes);
+app.use('/api', apiRoutes);
 
 
 app.listen(secret.port, function(err){
